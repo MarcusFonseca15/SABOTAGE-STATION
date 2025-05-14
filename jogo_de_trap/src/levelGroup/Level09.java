6f0a2804279ddbccb53d70749e62b6149152a8ec
Marcus <marcusfonseca15@gmail.com>
2025-05-14 15:08:34 -0300

    fix: level6 e sla oq

--- jogo_de_trap/src/levelGroup/Level09.java
+++ jogo_de_trap/src/levelGroup/Level09.java
@@ -1,5 +1,6 @@
 package levelGroup;
 
+import jogo_de_trap.Laser;
 import jogo_de_trap.Level;
 import jogo_de_trap.Platform;
 import jogo_de_trap.Player;
@@ -7,18 +8,18 @@ import jogo_de_trap.Player;
 public class Level09 extends Level {
 
     private static int[][] mapa = {
-            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 },
+            { 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 35, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 36, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 36, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 36, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 37, 0 },
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+            { 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 1, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11 }
     };
 
     @Override
@@ -27,14 +28,49 @@ public class Level09 extends Level {
     }
 
     public Level09(Player player) {
-        super(3);
-        // TODO Auto-generated constructor stub
-        this.titulo = "0.";
+        super(9);
+        designTraps();
+        this.titulo = "09. Desafiar a Gravidade! ";
     }
 
     @Override
     protected void designTraps() {
 
+        Laser l1 = (Laser) mapaObjetos[4][14];
+        Laser l2 = (Laser) mapaObjetos[5][14];
+        Laser l3 = (Laser) mapaObjetos[6][14];
+        Laser l4 = (Laser) mapaObjetos[7][14];
+        Laser l5 = (Laser) mapaObjetos[8][14];
+        laserAnda(l1, 0, l1.getX(), 2);
+        laserAnda(l2, 0, l2.getX(), 2);
+        laserAnda(l3, 0, l3.getX(), 2);
+        laserAnda(l4, 0, l1.getX(), 2);
+        laserAnda(l5, 0, l1.getX(), 2);
+    }
+
+    private void laserAnda(Laser laser, int posMin, int posMax, int vel) {
+        new Thread(() -> {
+            try {
+                boolean indo = true;
+                while (true) {
+                    if (indo) {
+                        laser.setX(laser.getX() + vel);
+                        if (laser.getX() >= posMax) {
+                            indo = false;
+                        }
+                    } else {
+                        laser.setX(laser.getX() - vel);
+                        if (laser.getX() <= posMin) {
+                            indo = true;
+                        }
+                    }
+
+                    Thread.sleep(20); // suavidade do movimento
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
 }
