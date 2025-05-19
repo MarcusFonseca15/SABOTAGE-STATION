1de71094e6a9def93389562c175bd420e80b0b9f
Marcus <marcusfonseca15@gmail.com>
2025-05-18 21:12:47 -0300

    feat: startPanel

--- jogo_de_trap/src/jogo_de_trap/GameFrame.java
+++ jogo_de_trap/src/jogo_de_trap/GameFrame.java
@@ -4,11 +4,16 @@ import javax.swing.*;
 
 public class GameFrame extends JFrame {
     public GameFrame() {
-        this.add(new GamePanel());
+        // this.add(new GamePanel());
         this.setTitle("Sabotage Station");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
+
+        StartPanel startPanel = new StartPanel(this);
+        this.add(startPanel);
+
         this.pack();
+        this.setSize(800, 637);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
