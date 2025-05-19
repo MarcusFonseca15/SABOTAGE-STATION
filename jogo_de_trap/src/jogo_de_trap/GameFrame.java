cd4bd864d3762916a563050ca98d2489967cbf47
Marcus <marcusfonseca15@gmail.com>
2025-05-18 21:46:02 -0300

    feat FINAL: ADD MUSICA

--- jogo_de_trap/src/jogo_de_trap/GameFrame.java
+++ jogo_de_trap/src/jogo_de_trap/GameFrame.java
@@ -3,6 +3,9 @@ package jogo_de_trap;
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
+
+    Som musicBG;
+
     public GameFrame() {
         // this.add(new GamePanel());
         this.setTitle("Sabotage Station");
@@ -16,5 +19,12 @@ public class GameFrame extends JFrame {
         this.setSize(800, 637);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
+
+        tocarMusica();
+    }
+
+    private void tocarMusica() {
+        musicBG = new Som();
+        musicBG.tocar("jogo_de_trap/src/res/musica.wav", -10.0f); // volume em dB
     }
 }
